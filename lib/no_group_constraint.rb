85ba358bf5c22ff2312aa844c0a41a3d9a4d492f
Jarek Radosz <jarek@cvx.dev>
2025-03-28 00:12:00 +0100

    bump

--- lib/no_group_constraint.rb
+++ lib/no_group_constraint.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class NoGroupConstraint
   def matches?(request)
     return false unless request.format.html?
--- plugin.rb
+++ plugin.rb
@@ -4,15 +4,7 @@
 # version: 0.1
 # authors: Neil Lalonde
 
-register_css <<CSS
-.wordpress-preferences-link {
-  margin-top: 32px;
-  margin-bottom: 24px;
-}
-.wordpress-preferences-link a {
-  text-decoration: underline;
-}
-CSS
+register_asset "stylesheets/common.scss"
 
 after_initialize do
   require_dependency File.expand_path("../lib/no_group_constraint.rb", __FILE__)
